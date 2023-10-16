0adfd369938da4b1f4b8431874f83e8f01e4edca
majorli <mingjiang.li@iluvatar.com>
2023-10-16 08:49:12 +0000

    !251 Create mmcv v2.0.0rc4 version install path and unify * update mobileone byol mocov2 mmcv path in readme doc * update CWD README.md doc style * move mmcv v2.0.0rc4 version to toolbox/ * add v1.5.3 mmcv version

--- cv/distiller/CWD/pytorch/mmcv/tests/data/config/deprecated_as_base.py
+++
@@ -1,2 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-_base_ = './deprecated.py'
--- cv/distiller/CWD/pytorch/mmcv/tests/data/config/q.py
+++
@@ -1,2 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-custom_imports = dict(imports=['r'], allow_failed_imports=False)
